8c5b09dc15be0195a22be3cad1dfdf32d48df7c3
Mohammad <smrh68@gmail.com>
2024-07-13 14:45:14 +0330

    set controller, route, middleware and check api is working

---
+++ app/Http/Middleware/LoginRandomUser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class LoginRandomUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $id = rand(1, 10);
+        Auth::loginUsingId($id);
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\v1\LotteryController;
+use App\Http\Middleware\LoginRandomUser;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/v1/lucky-wheel', [LotteryController::class, 'spin'])->middleware(LoginRandomUser::class);
