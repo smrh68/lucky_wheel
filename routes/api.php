d1895d0b7b292061f3520b8cddb5b1f8bb62611c
Mohammad <smrh68@gmail.com>
2024-07-15 18:07:40 +0330

    add rate limiter

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\Api\v1\LotteryController;
 use App\Http\Middleware\LoginRandomUser;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/v1/lucky-wheel', [LotteryController::class, 'spin'])->middleware(LoginRandomUser::class);
+Route::get('/v1/lucky-wheel', [LotteryController::class, 'spin'])
+    ->middleware([LoginRandomUser::class, 'throttle:api']);
